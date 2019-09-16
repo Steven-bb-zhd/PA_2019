bd7e4a9771d6af3ef09b8a2bc862ad82d3f92d73
monitor <tracer@njuics.org>
2019-09-16 10:29:01 +0800

    git monitor commit.

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -197,6 +197,21 @@ uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
 #endif
 }
 
+void set_CF_sub(uint32_t result, uint32_t dest,size_t data_size)
+{
+		if(data_size==8)
+	{
+		dest=dest&0xff;
+		result=result&0xff;
+	}
+	else if(data_size==16)
+	{
+		dest=dest&0xffff;
+		result=result&0xffff;
+	}
+	cpu.eflags.CF = dest < result;
+}
+
 uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size)
 {
 #ifdef NEMU_REF_ALU
