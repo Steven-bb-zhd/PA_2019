1a5c94756681dc14e97fe17c16752cb4aa2a5ca8
monitor <tracer@njuics.org>
2019-09-16 21:47:45 +0800

    git monitor commit.

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -405,6 +405,10 @@ uint32_t alu_or(uint32_t src, uint32_t dest, size_t data_size)
 
 uint32_t alu_shl(uint32_t src, uint32_t dest, size_t data_size)
 {
+	uint32_t res=0;
+	if (src==0){
+		
+	}
 #ifdef NEMU_REF_ALU
 	return __ref_alu_shl(src, dest, data_size);
 #else
