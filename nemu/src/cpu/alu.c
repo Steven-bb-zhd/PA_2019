7bebed2cd0c302310947cb9bc27092d2b5b50cdb
tracer-pa-public <tracer@njuics.org>
2019-09-17 18:47:33 +0800

    >  nemu 181860149 stevenzhd Linux debian-zhd-PA 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  18:47:33 up 42 min,  1 user,  load average: 1.62, 1.37, 1.12 74ad683b6c48cf28491ba877914f1af6f80ac32b

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -668,7 +668,7 @@ uint32_t alu_sar(uint32_t src, uint32_t dest, size_t data_size)
 		set_ZF(res,data_size);
 		set_SF(res,data_size);
 		return res;
-	}vvvvv
+	}
 #ifdef NEMU_REF_ALU
 	return __ref_alu_sar(src, dest, data_size);
 #else
