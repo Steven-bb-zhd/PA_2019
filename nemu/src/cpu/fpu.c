f3e77d0389ee4b29391d19747aca08266774c19c
monitor <tracer@njuics.org>
2019-09-18 13:54:01 +0800

    git monitor commit.

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -106,8 +106,8 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 		//printf("\e[0;31mPlease implement me at fpu.c\e[0m\n");
 		//assert(0);
 		uint32_t last_3_bits =sig_grs&0x7;
-		printf("sig_grs = %llx\n",sig_grs);
-		printf("last_3_bits = %x\n",last_3_bits);
+		//printf("sig_grs = %llx\n",sig_grs);
+		//printf("last_3_bits = %x\n",last_3_bits);
 		if(last_3_bits>4){
 			sig_grs>>=3;
 			//printf("sig_grs = %llx\n",sig_grs);
@@ -116,8 +116,8 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 			while((sig_grs>>23)>1){
 				sig_grs>>=1;
 				exp++;
-				printf("sig_grs = %llx\n",sig_grs);
-				printf("exp = %x\n",exp);
+				//printf("sig_grs = %llx\n",sig_grs);
+				//printf("exp = %x\n",exp);
 			}
 			//printf("sig_grs = %llx\n",sig_grs);
 			//printf("exp = %x\n",exp);
@@ -135,7 +135,7 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 		}
 		else{
 			sig_grs >>=3;
-			printf("sig_grs = %llx\n",sig_grs);
+			//printf("sig_grs = %llx\n",sig_grs);
 		}
 		sig_grs&=0x7fffff;
 		//printf("sig_grs = %llx\n",sig_grs);
