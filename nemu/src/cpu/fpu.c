b88a0273c6c454bf3bfdce02714e4d75f4680793
tracer-pa-public <tracer@njuics.org>
2019-09-18 13:30:59 +0800

    >  nemu 181860149 stevenzhd Linux debian-zhd-PA 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  13:30:59 up  1:25,  1 user,  load average: 0.25, 0.52, 0.72 34f29bbbc50cf33eec90a1bdfe8c876b6cb6eb77

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -76,7 +76,7 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 			//printf("\e[0;31mPlease implement me at fpu.c\e[0m\n");
 			//assert(0);
 			sig_grs<<=1;
-			printf("sig_grs = %llx\n",sig_grs>>26);
+			printf("sig_grs = %llx\n",sig_grs);
 			exp--;
 		}
 		printf("sig_grs = %llx\n",sig_grs>>26);
