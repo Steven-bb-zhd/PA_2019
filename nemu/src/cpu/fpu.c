12c7f535bbb895e59a5153f1b44c115a3e330005
tracer-pa-public <tracer@njuics.org>
2019-09-19 00:50:26 +0800

    >  clean 181860149 stevenzhd Linux debian-zhd-PA 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  00:50:26 up  2:13,  1 user,  load average: 0.75, 0.32, 0.32 5fa51b86acecf06a23a773da272f7300d2b32d3a

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -172,7 +172,8 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 	f.sign = sign;
 	f.exponent = (uint32_t)(exp & 0xff);
 	f.fraction = sig_grs; // here only the lowest 23 bits are kept
-	//printf("sig_grs = %llx\n",sig_grs);
+	printf("sig_grs = %llx\n",sig_grs);
+	printf("f.exponent=%x\n",f.exponent);
 	//printf("float add f.val= %x\n",f.val);
 	return f.val;
 }
