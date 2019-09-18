dfab97670d159c1a549931cb11803bc4b17447e2
tracer-pa-public <tracer@njuics.org>
2019-09-18 13:35:33 +0800

    >  nemu 181860149 stevenzhd Linux debian-zhd-PA 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  13:35:33 up  1:30,  1 user,  load average: 0.55, 0.49, 0.65 4c51b56228e5102ce423d1edddb010bc71b1d15

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -111,6 +111,7 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 		if(last_3_bits>4){
 			sig_grs>>=3;
 			sig_grs++;
+			printf("sig_grs = %llx\n",sig_grs>>26);
 			while((sig_grs>>(23+3))>1){
 				sig_grs>>=1;
 				exp++;
