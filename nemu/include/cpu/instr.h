174ab1a35366cdf0f491993c4b0e3f28b7331be9
monitor <tracer@njuics.org>
2019-10-18 01:54:01 +0800

    git monitor commit.

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -26,5 +26,5 @@ extern uint8_t data_size;
 int jmp_near(uint32_t eip,uint8_t opcode);
 int push_ebp_v(uint32_t eip,uint8_t opcode);
 int push_ebx_v(uint32_t eip,uint8_t opcode);
-//int group_1_bv(uint32_t eip,uint8_t opcode);
+int group_1_bv(uint32_t eip,uint8_t opcode);
 #endif
