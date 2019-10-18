9a002f0dd1dfc9f493f425d8c2b5f64a3ed09f17
monitor <tracer@njuics.org>
2019-10-18 15:10:02 +0800

    git monitor commit.

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -31,5 +31,5 @@ int add_i2rm_bv(uint32_t eip,uint8_t opcode);
 int adc_i2rm_bv(uint32_t eip,uint8_t opcode);
 int or_i2rm_bv(uint32_t eip,uint8_t opcode);
 int sub_i2rm_bv(uint32_t eip,uint8_t opcode);
-
+int sbb_i2rm_bv(uint32_t eip,uint8_t opcode);
 #endif
---
+++ nemu/src/cpu/instr/and.c
@@ -0,0 +1,22 @@
+#include "cpu/instr.h"
+
+make_instr_func(and_i2rm_bv){
+    OPERAND imm,rm;
+    int len=1;
+    imm.data_size=8;
+    rm.data_size=data_size;
+    len+=modrm_opcode_rm(eip+1,&opcode,&rm);
+    operand_read(&rm);
+
+    imm.type=OPR_IMM;
+    imm.addr=eip+len;
+    len+=1;
+    operand_read(&imm);
+
+    imm.val=sign_ext(imm.val,imm.data_size);
+    rm.val=sign_ext(rm.val,rm.data_size);
+    rm.val=alu_and(imm.val,rm.val,rm.data_size);
+    operand_write(&rm);
+    print_asm_2("adn","",len,&imm,&rm);
+    return len;
+}
