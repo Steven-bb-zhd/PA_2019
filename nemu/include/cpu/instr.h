0e0cd81fc9bb70ba7c690b378cd6fdbd46bcb1f8
monitor <tracer@njuics.org>
2019-10-18 15:11:01 +0800

    git monitor commit.

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -32,4 +32,5 @@ int adc_i2rm_bv(uint32_t eip,uint8_t opcode);
 int or_i2rm_bv(uint32_t eip,uint8_t opcode);
 int sub_i2rm_bv(uint32_t eip,uint8_t opcode);
 int sbb_i2rm_bv(uint32_t eip,uint8_t opcode);
+int and_i2rm_bv(uint32_t eipp,uint8_t opcode);
 #endif
