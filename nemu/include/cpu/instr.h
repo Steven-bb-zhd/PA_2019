9071bef0833216599f1cd767986f5dcbea15835a
monitor <tracer@njuics.org>
2019-10-18 21:41:01 +0800

    git monitor commit.

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -43,4 +43,5 @@ int or_i2rm_v(uint32_t eip,uint8_t opcode);
 int adc_i2rm_v(uint32_t eip,uint8_t opcode);
 int sbb_i2rm_v(uint32_t eip,uint8_t opcode);
 int and_i2rm_v(uint32_t eip,uint8_t opcode);
+int sub_i2rm_v(uint32_t eip,uint8_t opcode);
 #endif
--- nemu/src/cpu/instr/sub.c
+++ nemu/src/cpu/instr/sub.c
@@ -1,5 +1,14 @@
 #include "cpu/instr.h"
 
+static void instr_execute_2op(){
+    operand_read(&opr_src);
+    operand_read(&opr_dest);
+    opr_src.val=sign_ext(opr_src.val,opr_src.data_size);
+    opr_dest.val=sign_ext(opr_dest.val,opr_dest.data_size);
+    opr_dest.val=alu_sub(opr_src.val,opr_dest.val,data_size);
+    operand_write(&opr_dest);
+}
+
 make_instr_func(sub_i2rm_bv){
     OPERAND imm,rm;
     int len=1;
@@ -21,3 +30,5 @@ make_instr_func(sub_i2rm_bv){
     print_asm_2("sub","",len,&imm,&rm);
     return len;
 }
+
+make_instr_impl_2op(sub,i,rm,v)
