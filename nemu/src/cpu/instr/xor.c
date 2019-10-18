b1f71e6115e455c07f741d03370d67c8e55efa01
monitor <tracer@njuics.org>
2019-10-18 21:42:02 +0800

    git monitor commit.

--- nemu/src/cpu/instr/xor.c
+++ nemu/src/cpu/instr/xor.c
@@ -1,5 +1,15 @@
 #include "cpu/instr.h"
 
+static void instr_execute_2op(){
+    operand_read(&opr_src);
+    operand_read(&opr_dest);
+    opr_src.val=sign_ext(opr_src.val,opr_src.data_size);
+    opr_dest.val=sign_ext(opr_dest.val,opr_dest.data_size);
+    opr_dest.val=alu_xor(opr_src.val,opr_dest.val,data_size);
+    operand_write(&opr_dest);
+}
+
+
 make_instr_func(xor_i2rm_bv){
     OPERAND imm,rm;
     int len=1;
@@ -20,3 +30,5 @@ make_instr_func(xor_i2rm_bv){
     print_asm_2("xor","",len,&imm,&rm);
     return len;
 }
+
+make_instr_impl_2op(xor,i,rm,v)
