5d67ac6bc559d034fea8d0bbdf061e563a5dbd12
monitor <tracer@njuics.org>
2019-10-18 15:18:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/cmps.c
+++ nemu/src/cpu/instr/cmps.c
@@ -38,3 +38,25 @@ make_instr_func(cmps_b)
 	print_asm_0("cmps (%%edi), (%%esi)", "", 1);
 	return 1;
 }
+
+make_instr_func(cmp_i2rm_bv){
+	OPERAND imm,rm;
+    int len=1;
+    imm.data_size=8;
+    rm.data_size=data_size;
+    len+=modrm_opcode_rm(eip+len,&opcode,&rm);
+    operand_read(&rm);
+
+    imm.type=OPR_IMM;
+    imm.addr=eip+len;
+    len+=1;
+    operand_read(&imm);
+
+    imm.val=sign_ext(imm.val,imm.data_size);
+    rm.val=sign_ext(rm.val,rm.data_size);
+
+    rm.val=alu_sub(imm.val,rm.val,rm.data_size);
+    operand_read(&rm);
+    print_asm_2("cmp_bv","",len,&imm,&rm);
+    return len;
+}
