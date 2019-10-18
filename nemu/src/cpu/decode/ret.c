4a24753fb08a94ac962108b234af8ff5b06493a2
tracer-pa-public <tracer@njuics.org>
2019-10-18 19:40:29 +0800

    >  nemu 181860149 stevenzhd Linux debian-zhd-PA 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  19:40:29 up  1:44,  1 user,  load average: 1.66, 1.12, 1.05 3fc865fd6b43187710b5f3ff87218a7adc4934

--- nemu/src/cpu/decode/ret.c
+++ nemu/src/cpu/decode/ret.c
@@ -6,7 +6,7 @@ make_instr_func(ret_near){
     opr_eip.data_size=data_size;
     printf("cpu.esp=%x\n",cpu.esp);
     opr_eip.addr=cpu.esp;
-    opr_eip.sreg=SREG_SS;
+    opr_eip.sreg=SREG_CS;
     operand_read(&opr_eip);
     printf("eip=%x\n",opr_eip.val);
     cpu.esp+=data_size/8;
