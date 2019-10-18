1b36d5e691ccdc2e64ba897f3830b56f3e2e8d77
monitor <tracer@njuics.org>
2019-10-18 14:18:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/group.c
+++ nemu/src/cpu/instr/group.c
@@ -21,25 +21,25 @@
 #define make_group_impl_cond make_group_impl
 #endif
 
-make_group_impl_cond(group_1_b);
-make_group_impl_cond(group_1_v);
-make_group_impl_cond(group_1_bv);
-make_group_impl_cond(group_2_b);
-make_group_impl_cond(group_2_v);
-make_group_impl_cond(group_2_1b);
-make_group_impl_cond(group_2_1v);
-make_group_impl_cond(group_2_cb);
-make_group_impl_cond(group_2_cv);
-make_group_impl_cond(group_3_b);
-make_group_impl_cond(group_3_v);
-make_group_impl_cond(group_5_indirect);
-make_group_impl_cond(group_7);
-make_group_impl_cond(group_x87_d8);
-make_group_impl_cond(group_x87_d9);
-make_group_impl_cond(group_x87_da);
-make_group_impl_cond(group_x87_db);
-make_group_impl_cond(group_x87_dc);
-make_group_impl_cond(group_x87_dd);
-make_group_impl_cond(group_x87_de);
-make_group_impl_cond(group_x87_df);
+make_group_impl_cond(group_1_b)
+make_group_impl_cond(group_1_v)
+make_group_impl_cond(group_1_bv)
+make_group_impl_cond(group_2_b)
+make_group_impl_cond(group_2_v)
+make_group_impl_cond(group_2_1b)
+make_group_impl_cond(group_2_1v)
+make_group_impl_cond(group_2_cb)
+make_group_impl_cond(group_2_cv)
+make_group_impl_cond(group_3_b)
+make_group_impl_cond(group_3_v)
+make_group_impl_cond(group_5_indirect)
+make_group_impl_cond(group_7)
+make_group_impl_cond(group_x87_d8)
+make_group_impl_cond(group_x87_d9)
+make_group_impl_cond(group_x87_da)
+make_group_impl_cond(group_x87_db)
+make_group_impl_cond(group_x87_dc)
+make_group_impl_cond(group_x87_dd)
+make_group_impl_cond(group_x87_de)
+make_group_impl_cond(group_x87_df)
 
