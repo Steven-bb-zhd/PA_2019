b38aa1bf1b28ff7b328a9786267490c28963f7a4
monitor <tracer@njuics.org>
2019-10-18 19:26:01 +0800

    git monitor commit.

--- nemu/src/cpu/decode/ret.c
+++ nemu/src/cpu/decode/ret.c
@@ -5,6 +5,7 @@ make_instr_func(ret_near){
     opr_eip.type=OPR_MEM;
     opr_eip.data_size=data_size;
     opr_eip.addr=cpu.esp;
+    opr_eip.sreg=SREG_SS;
     operand_read(&opr_eip);
     cpu.esp+=data_size/8;
     cpu.eip=opr_eip.val;
