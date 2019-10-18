17798a786b504332e4a8405bc37995eccf4f1775
monitor <tracer@njuics.org>
2019-10-18 22:49:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/jcc.c
+++ nemu/src/cpu/instr/jcc.c
@@ -29,3 +29,17 @@ static void instr_execute_1op_cc_fail() {/* Do nothing */}
 jcc_helper(near)
 jcc_helper(short_)
 make_instr_impl_1op_cc(j, i, short_, ecxz)
+
+make_instr_func(jna_short){
+	OPERAND imm;
+	int len=1;
+	imm.type=OPR_IMM;
+	imm.data_size=8;
+	imm.addr=eip+1;
+	operand_read(&imm);
+	len+=1;
+	if(cpu.eflags.CF==1||cpu.eflags.ZF==1){
+		len+=(int8_t)imm.val;
+	}
+	return len;
+}
