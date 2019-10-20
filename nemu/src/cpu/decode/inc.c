b50fe063a908d24e94d7551a69b596733beed6db
monitor <tracer@njuics.org>
2019-10-20 23:46:01 +0800

    git monitor commit.

--- nemu/src/cpu/decode/inc.c
+++ nemu/src/cpu/decode/inc.c
@@ -1,5 +1,11 @@
 #include "cpu/instr.h"
 
+static void instr_execute_1op(){
+    operand_read(&opr_src);
+    opr_src.val=alu_add(1,opr_src.val,data_size);
+    operand_write(&opr_src);
+}
+
 make_instr_func(inc_rm_v){
     OPERAND rm;
     int len=1;
