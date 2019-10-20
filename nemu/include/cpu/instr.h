d2a14e15b935d51fa8d659db8dc6f2bd337eb4bb
tracer-pa-public <tracer@njuics.org>
2019-10-20 22:46:34 +0800

    >  nemu 181860149 stevenzhd Linux debian-zhd-PA 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  22:46:34 up  5:49,  1 user,  load average: 1.30, 0.83, 0.75 d9038251f64160b599f05e240770c02f8d56475

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -49,4 +49,5 @@ int cmp_i2rm_v(uint32_t eip,uint8_t opcode);
 int jmp_short(uint32_t eip,uint8_t opcode);
 int jna_short(uint32_t eip,uint8_t opcode);
 int lea(uint32_t eip,uint8_t opcode);
+int cmp_r2rm_v(uint32_t eip,uint8_t opcode);
 #endif
