52ee69d85ca41e801d3d4f9c1c75aae1192835c6
monitor <tracer@njuics.org>
2019-10-20 20:06:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/lea.c
+++ nemu/src/cpu/instr/lea.c
@@ -1,5 +1,13 @@
 #include "cpu/instr.h"
 
-/*/make_instr_func(lea_v) {
-   OPERAND rm_1,rm_2;
-} */
+make_instr_func(lea) {
+   OPERAND m,r;
+   int len=1;
+   r.data_size=data_size;
+   m.data_size=data_size;
+   len+=modrm_r_rm(eip+1,&r,&m);
+   operand_read(&m);
+   r.addr=m.addr;
+   operand_write(&r);
+   return len;
+}
