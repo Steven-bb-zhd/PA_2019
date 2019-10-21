9687485c45eb7be82409e96e3f3306f81bdb4f7b
monitor <tracer@njuics.org>
2019-10-22 00:31:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/pop.c
+++ nemu/src/cpu/instr/pop.c
@@ -28,3 +28,22 @@ make_instr_func(pop_ebx_v){
     }
     return len;
 }
+
+make_instr_func(pop_ebp_v){
+    OPERAND opr_ebp;
+    int len=1;
+    opr_ebp.type=OPR_MEM;
+    opr_ebp.data_size=data_size;
+    opr_ebp.sreg=SREG_SS;
+    opr_ebp.addr=cpu.esp;
+    cpu.ebp+=data_size/8;
+    operand_read(&opr_ebp);
+    if(data_size==16){
+        cpu.ebp=(opr_ebp.val&0xffff)|(cpu.ebp&0xffff0000);
+    }
+    else
+    {
+        cpu.ebp=opr_ebp.val;
+    }
+    return len;
+}
