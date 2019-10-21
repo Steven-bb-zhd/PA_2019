47ee72bf807713604c72baa091d4e81c4fceb291
tracer-pa-public <tracer@njuics.org>
2019-10-21 17:43:07 +0800

    >  nemu 181860149 stevenzhd Linux debian-zhd-PA 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  17:43:07 up  2:28,  1 user,  load average: 1.13, 1.19, 1.08 1c03fbf1ef906c9ec061a45792ac107a5c0e7b35

--- nemu/src/cpu/instr/jcc.c
+++ nemu/src/cpu/instr/jcc.c
@@ -81,8 +81,6 @@ make_instr_func(jle_b){
 	imm.addr=eip+1;
 	operand_read(&imm);
 	len+=1;
-	operand_read(&imm);
-	len+=1;
 	print_asm_1("jle_b","",len,&imm);
 	if(cpu.eflags.ZF==1&&(cpu.eflags.SF!=cpu.eflags.OF)){
 		len+=(int8_t)imm.val;
