955baacdef19f99b6231ddfa44464452c0e4a8ae
tracer-pa-public <tracer@njuics.org>
2019-10-21 14:28:09 +0800

    >  nemu 181860149 stevenzhd Linux debian-zhd-PA 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  14:28:09 up 54 min,  1 user,  load average: 0.22, 0.66, 0.80 e50cc0579e7da58125eb40c2b9e4dd43735a350b

--- nemu/src/cpu/instr/lea.c
+++ nemu/src/cpu/instr/lea.c
@@ -3,14 +3,14 @@
 make_instr_func(lea) {
    OPERAND m,r;
    int len=1;
-   r.type=OPR_MEM;
+   r.type=OPR_REG;
    m.type=OPR_MEM;
    r.data_size=data_size;
    m.data_size=data_size;
    len+=modrm_r_rm(eip+1,&r,&m);
    operand_read(&m);
    printf("m.addr=%x\n",m.addr);
-   r.val=m.val;
+   r.val=m.addr;
    print_asm_2("lea","",len,&m,&r);
    operand_write(&r);
    return len;
