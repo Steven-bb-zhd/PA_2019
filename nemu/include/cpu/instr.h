da0b47c5322c7b8bedf9768c4e49fc572cdbe3d2
monitor <tracer@njuics.org>
2019-10-22 00:12:01 +0800

    git monitor commit.

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -70,5 +70,5 @@ int push_edx_v(uint32_t eip,uint8_t opcode);
 int push_r_v(uint32_t eip,uint8_t opcode);
 int push_eax_v(uint32_t eip,uint8_t opcode);
 int adc_r2rm_v(uint32_t eip,uint8_t opcode);
-int adc_i2a_v(uint32_t eip,uint8_t opcode);;
+int add_i2a_v(uint32_t eip,uint8_t opcode);
 #endif
