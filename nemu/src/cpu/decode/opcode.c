35daf562ae3ae65e545ee6cd82debbc2005bfe8c
tracer-pa-public <tracer@njuics.org>
2019-10-21 23:27:51 +0800

    >  nemu 181860149 stevenzhd Linux debian-zhd-PA 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  23:27:51 up  3:20,  1 user,  load average: 0.73, 0.65, 0.86 b134f9a39b64fdf98d93e4e16cfea851f6931363

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -21,7 +21,7 @@ instr_func opcode_entry[256] = {
     /* 0x44 - 0x47*/ inv, inv, inv, inv,
     /* 0x48 - 0x4b*/ inv, inv, inv, inv,
     /* 0x4c - 0x4f*/ inv, inv, inv, inv,
-    /* 0x50 - 0x53*/ push_ecx_v, inv, inv, push_ebx_v,
+    /* 0x50 - 0x53*/ inv, push_ecx_v, inv, push_ebx_v,
     /* 0x54 - 0x57*/ inv, push_ebp_v, push_esi_v, push_edi_v,
     /* 0x58 - 0x5b*/ inv, inv, inv, inv,
     /* 0x5c - 0x5f*/ inv, inv, inv, inv,
