3f12b014bf0d36d50e622a4e9ba153970c7946d4
monitor <tracer@njuics.org>
2019-10-22 21:43:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/cmps.c
+++ nemu/src/cpu/instr/cmps.c
@@ -9,6 +9,7 @@ static void instr_execute_2op(){
 }
 
 make_instr_impl_2op(cmp,i,rm,v)
+make_instr_impl_2op(cmp,i,rm,b)
 make_instr_impl_2op(cmp,r,rm,v)
 make_instr_impl_2op(cmp,rm,r,v)
 make_instr_impl_2op(cmp,i,a,b)
