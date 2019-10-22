34825752e990314320cc3a81f04105dce6e1d9e8
monitor <tracer@njuics.org>
2019-10-22 21:24:02 +0800

    git monitor commit.

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -88,5 +88,7 @@ int jl_near(uint32_t eip,uint8_t opcode);
 int jle_near(uint32_t eip,uint8_t opcode);
 int sub_r2rm_v(uint32_t eip,uint8_t opcode);
 int and_rm2r_v(uint32_t eip,uint8_t opcode);
+int and_rm2r_b(uint32_t eip,uint8_t opcode);
 int and_r2rm_v(uint32_t eip,uint8_t opcode);
+int and_r2rm_b(uint32_t eip,uint8_t opcode);
 #endif
