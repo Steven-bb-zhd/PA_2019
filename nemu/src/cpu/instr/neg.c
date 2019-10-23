c86255a867629b744e260c28f559d97dbac22b5f
monitor <tracer@njuics.org>
2019-10-23 11:21:02 +0800

    git monitor commit.

---
+++ nemu/src/cpu/instr/neg.c
@@ -0,0 +1,14 @@
+#include "cpu/instr.h"
+
+static void instr_execute_1op(){
+    operand_read(&opr_src);
+    if(opr_src.val==0){
+        cpu.eflags.CF=0;
+    }
+    else
+    {
+        cpu.eflags.CF=1;
+    }
+    opr_dest.val=-opr_src.val;
+    operand_write(&opr_dest);
+}
