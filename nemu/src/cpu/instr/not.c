063a9edc31412634314590fd7542a37e33646123
tracer-pa-public <tracer@njuics.org>
2019-10-23 11:56:55 +0800

    >  nemu 181860149 stevenzhd Linux debian-zhd-PA 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  11:56:55 up 12:33,  1 user,  load average: 1.72, 0.81, 0.76 3e3e2c2672257d446ffb3e1f398a9ce84bb92ea

--- nemu/src/cpu/instr/not.c
+++ nemu/src/cpu/instr/not.c
@@ -2,8 +2,9 @@
 
 static void instr_execute_1op(){
     operand_read(&opr_src);
-    opr_dest.val=!opr_src.val;
-    operand_write(&opr_dest);
+    opr_src.val=sign_ext(opr_src.val,opr_src.data_size);
+    opr_src.val=~opr_src.val;
+    operand_write(&opr_src);
 }
 
 make_instr_impl_1op(not,rm,v)
