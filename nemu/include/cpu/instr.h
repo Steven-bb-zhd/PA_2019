e81f53a38ad40e43170905c1424cd2818d0d3f86
monitor <tracer@njuics.org>
2019-10-23 11:27:01 +0800

    git monitor commit.

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -108,4 +108,12 @@ int or_rm2r_b(uint32_t eip,uint8_t opcode);
 int not_rm_v(uint32_t eip,uint8_t opcode);
 int neg_rm_v(uint32_t eip,uint8_t opcode);
 int neg_rm_b(uint32_t eip,uint8_t opcode);
+int mul_rm2a_v(uint32_t eip,uint8_t opcode);
+int mul_rm2a_b(uint32_t eip,uint8_t opcode);
+int imul_rm2a_v(uint32_t eip,uint8_t opcode);
+int imul_rm2a_b(uint32_t eip,uint8_t opcode);
+int idiv_rm2a_v(uint32_t eip,uint8_t opcode);
+int idiv_rm2a_b(uint32_t eip,uint8_t opcode);
+int div_rm2a_v(uint32_t eip,uint8_t opcode);
+int div_rm2a_b(uint32_t eip,uint8_t opcode);
 #endif
