0fcc6f837310e0e17f039cff30c9ee33d5ffc0a8
monitor <tracer@njuics.org>
2019-10-23 14:14:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/pop.c
+++ nemu/src/cpu/instr/pop.c
@@ -1,7 +1,7 @@
 #include "cpu/instr.h"
 
 static void instr_execute_1op(){
-    opr_dest.addr=cpu.esp;
+    opr_src.addr=cpu.esp;
     operand_read(&opr_src);
     cpu.esp+=data_size/8;
     opr_dest.val=opr_src.val;
@@ -34,27 +34,6 @@ make_instr_func(pop_ebx_v){
 
 make_instr_func(pop_ebp_v){
     OPERAND opr_ebp;
-    int len=1;
-    opr_ebp.type=OPR_MEM;
-    opr_ebp.data_size=data_size;
-    opr_ebp.sreg=SREG_SS;
-    opr_ebp.addr=cpu.esp;
-    operand_read(&opr_ebp);
-    cpu.esp+=data_size/8;
-    //printf("opr_ebp=%x\n",opr_ebp.val);
-    if(data_size==16){
-        cpu.ebp=(opr_ebp.val&0xffff)|(cpu.ebp&0xffff0000);
-    }
-    else
-    {
-        cpu.ebp=opr_ebp.val;
-    }
-    print_asm_0("pop    ebp","",len);
-    return len;
-}
-
-make_instr_func(pop_edi_v){
-    OPERAND opr_edi;
     int len=1;
     opr_ebp.type=OPR_MEM;
     opr_ebp.data_size=data_size;
