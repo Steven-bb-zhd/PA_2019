8798b80063647ba045eb4d6b11f608384553c4e5
monitor <tracer@njuics.org>
2019-10-23 16:17:01 +0800

    git monitor commit.

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -129,4 +129,5 @@ int cmp_rm2r_b(uint32_t eip,uint8_t opcode);
 int test_i2a_v(uint32_t eip,uint8_t opcode);
 int test_i2a_b(uint32_t eip,uint8_t opcode);
 int stos_b(uint32_t eip,uint8_t opcode);
+int cmps_v(uint32_t eip,uint8_t opcode);
 #endif
