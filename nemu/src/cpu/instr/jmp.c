e2f9824b90ce1bc68aaec4781c47182a2069c63c
tracer-pa-public <tracer@njuics.org>
2019-10-23 21:08:12 +0800

    >  nemu 181860149 stevenzhd Linux debian-zhd-PA 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  21:08:12 up 15:58,  1 user,  load average: 1.64, 1.11, 1.38 47d61ac4d0df12764909ec947d143c80e20aeace

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -5,7 +5,7 @@ make_instr_func(jmp_near)
         OPERAND rel;
         rel.type = OPR_IMM;
         rel.sreg = SREG_CS;
-        rel.data_size = 32;
+        rel.data_size = data_size;
         rel.addr = eip + 1;
 
         operand_read(&rel);
