f498b3fc20aee64d1ed22b22fde31ce910adc90d
monitor <tracer@njuics.org>
2019-11-01 15:04:01 +0800

    git monitor commit.

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -41,10 +41,10 @@ uint32_t loader()
 			//panic("Please implement the loader");
 
 /* TODO: copy the segment from the ELF file to its proper memory area */
-			memcpy(ph->p_vaddr,elf+ph->p_offset,ph->p_filesz);
+			memcpy((void*)ph->p_vaddr,(void*)elf+ph->p_offset,ph->p_filesz);
 /* TODO: zeror the memory area [vaddr + file_sz, vaddr + mem_sz) */
 			if(ph->p_filesz<ph->p_memsz){
-				memset(ph->p_vaddr+ph->p_filesz,0,ph->p_memsz-ph->p_filesz);
+				memset((void*)ph->p_vaddr+ph->p_filesz,0,ph->p_memsz-ph->p_filesz);
 			}
 #ifdef IA32_PAGE
 			/* Record the program break for future use */
