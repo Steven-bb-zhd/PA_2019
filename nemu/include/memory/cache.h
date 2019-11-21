22c1fbcb97342936a67d23120faa3bd25d74211c
monitor <tracer@njuics.org>
2019-11-21 19:43:02 +0800

    git monitor commit.

--- nemu/include/memory/cache.h
+++ nemu/include/memory/cache.h
@@ -1,10 +1,10 @@
-#include "stdio.h"
 #include "nemu.h"
+#include "stdio.h"
 
-strcut Cacheline{
+typedef strcut {
     uint8_t valid_bit;
     uint32_t In_addr;
     uint32_t Group_Num;
     uint32_t tag;
     uint8_t data[64];
-};
+}Cacheline;
