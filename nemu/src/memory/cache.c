c7ab9a4700b02290e4921eca4a01d4fb1e2fadba
tracer-pa-public <tracer@njuics.org>
2019-11-23 17:06:59 +0800

    >  nemu 181860149 stevenzhd Linux debian-zhd-PA 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  17:06:59 up  2:45,  1 user,  load average: 2.45, 2.14, 1.69 10ff9657555ccbc58b5af483a1634ac7d757e048

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -57,30 +57,36 @@ uint32_t cache_read(paddr_t paddr , size_t len , Cacheline* cache){
             return res;
         }
     }
+    uint32_t key=0;
     if(!find){
-        uint32_t key=notfind(paddr,len,cache,tag);
-        uint32_t res=0;
-        memcpy(&res,(void*)cache_block[key].data+rel,len);
-        return res;
+        key=notfind(paddr,len,cache,tag);
     }
+    uint32_t res=0;
+    memcpy(&res,(void*)cache_block[key].data+rel,len);
+    return res;
 }
 
-void cache_write(paddr_t paddr , size_t len , uint32_t data, Cacheline * cache){
+void cache_write (paddr_t paddr , size_t len , uint32_t data, Cacheline * cache){
     hw_mem_write(paddr,len,data);
-    uint32_t group=(paddr>>6)&0x7f;
-    uint32_t sign=(paddr>>13)&0x7ffff;
+    uint32_t group_num=(paddr>>6)&0x7f;
+    uint32_t tag=(paddr>>13)&0x7ffff;
     uint32_t rel=paddr&0x3f;
     for(int i=0;i<8;++i){
-        if(cache_block[8*group+i].tag==sign&&cache_block[8*group+i].valid_bit){
-            if(rel+len<=64){
-                memcpy(cache_block[8*group+i].data+rel,&data,len);
+        if(cache_block[8*group_num+i].valid_bit&&(cache_block[8*group_num+i].tag==tag)){
+            if((len+rel)<=64){
+                memcpy(cache_block[8*group_num+i].data+rel,&data,len);
             }
             else{
-                uint32_t over_limit=0;
-                over_limit=64-rel;
-                uint32_t data2=0;
-                data2=data>>(8*over_limit);
-                cache_write(paddr+over_limit,len-over_limit,data2,cache);
+                uint32_t over=0;
+                over=len+rel-64;
+                uint32_t over_data=0;
+                if(over==3)
+                    over_data=(data&0xffffff00)>>8;
+                else if(over==2)
+                    over_data=(data&0xffff0000)>>16;
+                else if(over==1)
+                    over_data=(data&0xff000000)>>24;
+                cache_write(paddr+4-over,over,over_data,cache);
             }
         }
     }
