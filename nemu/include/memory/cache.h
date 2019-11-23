d79225b3ffd744d63d7b21c91262e1a710f636a1
tracer-pa-public <tracer@njuics.org>
2019-11-23 16:49:08 +0800

    >  clean 181860149 stevenzhd Linux debian-zhd-PA 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  16:49:08 up  2:27,  1 user,  load average: 1.44, 1.37, 1.30 1e72583bb2227b2f6e3dfb60877196236457e0d1

--- nemu/include/memory/cache.h
+++ nemu/include/memory/cache.h
@@ -13,4 +13,4 @@ typedef struct
 
 void init_cache();
 uint32_t cache_read (paddr_t paddr , size_t len , Cacheline* cache);
-void cache_write (paddr_t paddr , size_t len , uint32_t data, Cacheline * cache);
+//void cache_write (paddr_t paddr , size_t len , uint32_t data, Cacheline * cache);
