e001c68bab65d48b82d5aa53fecd24111d652b60
monitor <tracer@njuics.org>
2019-11-23 13:34:12 +0800

    git monitor commit.

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -2,10 +2,21 @@
 #include "time.h"
 #include "memory/memory.h"
 
+Cacheline cache_block[1024];
 
 void init_caache(){
     for(int i=0;i<1024;++i){
-        cache[i].valid_bit=0;
+        cache_block[i].valid_bit=0;
+    }
+}
+
+uint32_t addcache(paddr_t paddr , size_t len , Cacheline* cache){
+    uint32_t group=(paddr>>6)&0x7f;
+    for(int i=0;i<8;++i){
+        if(cache_block[8*group+i].valid_bit==0){
+            cache_block[8*group+i].valid_bit=1;
+            
+        }
     }
 }
 
@@ -15,5 +26,20 @@ uint32_t cache_read(paddr_t paddr , size_t len , Cacheline* cache){
     uint32_t rel=paddr&0x3f;
     if(rel==61||rel==62||rel==63){
         uint32_t res = hw_mem_read(paddr,len);
+        return res;
+    }
+    uint32_t group_addr=0;
+    bool find=false;
+    for(int i=0;i<8;++i){
+        if(cache_block[group*8+i].tag==sign){
+            if(cache_block[group*8+i].valid_bit){
+                find = true;
+                group_addr=group*8+i;
+                break;
+            }
+        }
+    }
+    if(!find){
+
     }
 }
