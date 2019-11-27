3c1fc70509ccfccc3c6cad833f762a30e45e1391
tracer-pa-public <tracer@njuics.org>
2019-11-27 20:34:21 +0800

    >  clean 181860149 stevenzhd Linux debian-zhd-PA 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  20:34:21 up  6:42,  1 user,  load average: 1.73, 1.72, 1.67 d6e170aa3d0861e39cdffdbf4b3d68a32bb51f57

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -19,5 +19,16 @@ void load_sreg(uint8_t sreg)
 	/* TODO: load the invisibile part of the segment register 'sreg' by reading the GDT.
 	 * The visible part of 'sreg' should be assigned by mov or ljmp already.
 	 */
-
+	SegDesc segdesc;
+	memcpy(&segdesc,hw_mem+cpu.gdtr.base+8*(cpu.segReg[sreg].index),8);
+	uint32_t base=((segdesc.base_31_24<<24)|(segdesc.base_23_16<<16)|(segdesc.base_15_0));
+	uint32_t limit=((segdesc.limit_19_16<<16)|(segdesc.limit_15_0));
+	assert(segdesc.present==1);
+	assert(segdesc.granularity==1);
+	assert(limit==0xfffff);
+	assert(base==0);
+	cpu.segReg[sreg].base=base;
+	cpu.segReg[sreg].limit=limit;
+	//cpu.segReg[sreg].type=segdesc.type;
+	cpu.segReg[sreg].privilege_level=segdesc.privilege_level;
 }
