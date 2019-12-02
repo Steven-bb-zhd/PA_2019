6fbbec1d8626c9c8ebe68e22c5cd1fbbfe964f40
tracer-pa-public <tracer@njuics.org>
2019-12-03 00:50:40 +0800

    >  nemu 181860149 stevenzhd Linux debian-zhd-PA 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  00:50:40 up  4:15,  1 user,  load average: 1.30, 1.04, 0.90 818bb6a6bdb185b84a573d6e76ac9321e65d2940

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -59,7 +59,7 @@ make_instr_func(jmp_rm_v){
 }
 
 make_instr_func(jmp_far_imm){
-        OPERAND seg,offset;
+       /* *OPERAND seg,offset;
         seg.data_size=16;
         seg.sreg=SREG_CS;
         seg.addr=eip+1+data_size/8;
@@ -80,5 +80,30 @@ make_instr_func(jmp_far_imm){
                 cpu.eip=offset.val;
         }
 
-        return 0;
+        return 0;*/
+        OPERAND selector, offset;
+	int len = 1;
+
+	selector.data_size = 16;
+	offset.data_size = data_size;
+
+	offset.type = OPR_IMM;
+	offset.addr = eip + len;
+	operand_read(&offset);
+	len += data_size/8;
+
+	selector.type = OPR_IMM;
+	selector.addr = eip + len;
+	operand_read(&selector);
+	len += 2;
+
+	cpu.cs.val = selector.val;
+	load_sreg((uint8_t)cpu.cs.index);
+	assert(data_size == 16 || data_size == 32);
+	if(data_size == 16)
+		cpu.eip = (offset.val & 0xffff);
+	else
+		cpu.eip = offset.val;
+	
+	return 0;
 }
