ace3bfd5f2505ef32653b57a13e07a021d14921b
tracer-pa-public <tracer@njuics.org>
2019-12-02 11:09:11 +0800

    >  clean 181860149 stevenzhd Linux debian-zhd-PA 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  11:09:11 up  7:02,  1 user,  load average: 1.46, 1.15, 1.04 8a88c1a1e6a4a53e9fceeaff549cfda02cafaecb

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -39,7 +39,7 @@ uint32_t loader()
 
 			// remove this panic!!!
 			//panic("Please implement the loader");
-			mm_malloc(ph->p_vaddr,ph->p_filesz);
+			mm_malloc(ph->p_vaddr,ph->p_memsz);
 /* TODO: copy the segment from the ELF file to its proper memory area */
 			memcpy((void*)ph->p_vaddr,(void*)elf+ph->p_offset,ph->p_filesz);
 /* TODO: zeror the memory area [vaddr + file_sz, vaddr + mem_sz) */
