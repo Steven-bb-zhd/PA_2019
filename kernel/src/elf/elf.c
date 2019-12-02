0b821051ac19f63e65d524b60644f1d97e7381dd
tracer-pa-public <tracer@njuics.org>
2019-12-02 12:44:24 +0800

    >  clean 181860149 stevenzhd Linux debian-zhd-PA 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  12:44:24 up  7:44,  1 user,  load average: 0.77, 0.60, 0.91 821c0c1fba9437b763ef0c32310e5c0533af474e

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -46,10 +46,10 @@ uint32_t loader()
 			//panic("Please implement the loader");
 			//uint32_t addr=mm_malloc(ph->p_vaddr,ph->p_memsz);
 /* TODO: copy the segment from the ELF file to its proper memory area */
-			memcpy((void*)addr,(void*)elf+ph->p_offset,ph->p_filesz);
+			memcpy((void*)addr,(void*)ph->p_offset,ph->p_filesz);
 /* TODO: zeror the memory area [vaddr + file_sz, vaddr + mem_sz) */
 			if(ph->p_filesz<ph->p_memsz){
-				memset((void*)addr+ph->p_filesz,0,ph->p_memsz-ph->p_filesz);
+				memset((void*)(addr+ph->p_filesz),0,ph->p_memsz-ph->p_filesz);
 			}
 #ifdef IA32_PAGE
 			/* Record the program break for future use */
