31c792368ef46bd3efdb8f3fd6e464382f0ed84e
tracer-pa-public <tracer@njuics.org>
2019-12-03 16:15:54 +0800

    >  nemu 181860149 stevenzhd Linux debian-zhd-PA 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  16:15:54 up  1:44,  1 user,  load average: 0.86, 0.83, 0.81 32289e641cff4df2c44c67bb70f7053a8abcb782

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -4,7 +4,7 @@ make_instr_func(call_near){
     OPERAND rel,opr_eip;
     //int len=1;
     rel.type=OPR_IMM;
-    //rel.sreg=SREG_CS;
+    rel.sreg=SREG_CS;
     rel.data_size=data_size;
     rel.addr=eip+1;
     operand_read(&rel);
@@ -31,7 +31,7 @@ make_instr_func(call_near_indirect){
     cpu.esp-=data_size/8;
     opr_eip.type=OPR_MEM;
     opr_eip.addr=cpu.esp;
-    opr_eip.sreg=SREG_SS;
+    //opr_eip.sreg=SREG_SS;
     opr_eip.val=eip+len;
     operand_write(&opr_eip);
     cpu.eip=rm.val;
