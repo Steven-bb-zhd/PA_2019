dd9ef435f3ee97411913b93bcecf6eba696236fd
tracer-pa-public <tracer@njuics.org>
2019-12-03 11:37:10 +0800

    >  nemu 181860149 stevenzhd Linux debian-zhd-PA 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  11:37:10 up 39 min,  1 user,  load average: 1.22, 1.02, 1.10 3a244c2b8b34bea63591759ff5ae3dfc93ea4d7f

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -5,24 +5,27 @@
 paddr_t page_translate(laddr_t laddr)
 {
 #ifndef TLB_ENABLED
-	uint32_t dir=(laddr>>22)&0x3ff;
-	printf("%x",dir);
-	uint32_t page=(laddr>>12)&0x3ff;
-	uint32_t offset=laddr&0xfff;
-	uint32_t dir_base=cpu.cr3.pdbr<<12;
-	uint32_t dir_entry=hw_mem_read(dir_base+dir,4);
-	uint32_t d_present=(dir_entry>>31)&0x1;
-	assert(d_present==1);
-	dir_entry&=0xfffff000;
-	uint32_t pg_base=dir_entry+page;
-	uint32_t pg_tbl=hw_mem_read(pg_base,4);
-	uint32_t p_present=(pg_tbl>>31)&0x1;
-	assert(p_present==1);
-	pg_tbl&=0xfffff000;
-	uint32_t res=pg_tbl+offset;
-	return res;
-	printf("\nPlease implement page_translate()\n");
-	assert(0);
+	uint32_t dir = (laddr & 0xffc00000) >> 22;
+	uint32_t page = (laddr & 0x003ff000) >> 12;
+	uint32_t offset = laddr & 0x00000fff;
+	uint32_t dirBase = cpu.cr3.pdbr << 12;
+	uint32_t pdeAddr = dirBase + dir * sizeof(PDE);
+	//printf("laddr: 0x%x\ndirBase: 0x%x\tpdeAddr: 0x%x\n", laddr, dirBase, pdeAddr);
+	PDE pdeRead;
+	pdeRead.val = paddr_read(pdeAddr, 4);
+	//printf("pdeRead.val: 0x%x\n", pdeRead.val);
+	assert(pdeRead.present == 1);
+	uint32_t pageBase = (pdeRead.page_frame << 12);
+	uint32_t pageAddr = pageBase + page * sizeof(PTE);
+	PTE pteRead;
+	pteRead.val = paddr_read(pageAddr, 4);
+	//printf("pteRead.val: 0x%x\n", pteRead.val);
+	assert(pteRead.present == 1);
+	uint32_t pageFrame = pteRead.page_frame;
+	paddr_t paddr = (pageFrame << 12) | offset;
+	//printf("paddr: 0x%x\n", paddr);
+	//printf("-------------------------------\n");
+	return paddr;
 #else
 	return tlb_read(laddr) | (laddr & PAGE_MASK);
 	;
