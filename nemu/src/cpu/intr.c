03a015f3debbf9178e5dfef666105ec8bf747885
tracer-pa-public <tracer@njuics.org>
2020-01-15 22:49:52 +0800

    >  clean 181860149 stevenzhd Linux debian-zhd-PA 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  22:49:52 up  3:24,  1 user,  load average: 1.30, 1.42, 1.09 161e729d6809bd59b3a6e16b6b9e1b1f539b3b44

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -5,49 +5,19 @@
 void raise_intr(uint8_t intr_no)
 {
 #ifdef IA32_INTR
-	OPERAND opr_eip,opr_cs,opr_eflags;
-
-	opr_eflags.data_size=32;
-	opr_eflags.type=OPR_MEM;
-	opr_eflags.sreg=SREG_SS;
-	opr_eflags.val=cpu.eflags.val;
+	uint32_t addr=(uint32_t)hw_mem+page_translate(segment_translate(cpu.idtr.base+8*intr_no,SREG_DS));
+	GateDesc* gatedesc=(void*)addr;
 	cpu.esp-=4;
-	opr_eflags.addr=cpu.esp;
-	operand_write(&opr_eflags);
-
-	opr_cs.data_size=16;
-	opr_cs.type=OPR_MEM;
-	opr_cs.sreg=SREG_SS;
-	opr_cs.val=cpu.cs.val;
+	vaddr_write(cpu.esp,SREG_DS,4,cpu.eflags.val);
 	cpu.esp-=2;
-	opr_cs.addr=cpu.esp;
-	operand_write(&opr_cs);
-
-	opr_eip.data_size=32;
-	opr_eip.type=OPR_MEM;
-	opr_eip.sreg=SREG_SS;
-	opr_eip.val=cpu.eip;
+	vaddr_write(cpu.esp,SREG_DS,2,cpu.cs.val);
 	cpu.esp-=4;
-	opr_eip.addr=cpu.esp;
-	operand_write(&opr_eip);
-
-	laddr_t idt_index=cpu.idtr.base+sizeof(GateDesc)*intr_no;
-	GateDesc gate;
-	gate.val[0]=laddr_read(idt_index,4);
-	gate.val[1]=laddr_read(idt_index+4,4);
-
-	if(gate.type == 0xe)
-		cpu.eflags.IF = 1;
-
-	uint32_t offset = (gate.offset_31_16 << 16) 
-						| gate.offset_15_0;
-	uint16_t selector = gate.selector;
-	cpu.cs.val = selector;
-	load_sreg((uint8_t)cpu.cs.index);
-
-	cpu.eip = offset;
-	printf("Please implement raise_intr()");
-	assert(0);
+	vaddr_write(cpu.esp,SREG_DS,4,cpu.eip);
+	if(gatedesc->type==0xe)
+		cpu.eflags.IF=1;
+	else if(gatedesc->type==0xf)
+		cpu.eflags.IF=0;
+	cpu.eip=(gatedesc->offset_31_16<<16)+gatedesc->offset_15_0;
 #endif
 }
 
