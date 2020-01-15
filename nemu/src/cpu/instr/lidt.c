e33bda5dcaf362e5c37a03d6db23c52b81bc4b9e
tracer-pa-public <tracer@njuics.org>
2020-01-15 23:21:48 +0800

    >  nemu 181860149 stevenzhd Linux debian-zhd-PA 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  23:21:48 up  3:56,  1 user,  load average: 1.42, 1.05, 0.94 ada2047c4796fae0840581023ae76744c8cfa709

--- nemu/src/cpu/instr/lidt.c
+++ nemu/src/cpu/instr/lidt.c
@@ -1,26 +1,24 @@
 #include "cpu/instr.h"
 
-make_instr_func(lidt){
-
+make_instr_func(lidt)
+{
     OPERAND rel;
-    rel.data_size=32;
-    rel.type=OPR_IMM;
-    rel.sreg=SREG_SS;
-    rel.addr=eip+2;
-    operand_read(&rel);
+    int len=1;
+    rel.data_size=data_size;
+    len+=modrm_rm(eip+1,&rel);
     uint16_t limit=0;
     uint32_t base=0;
-    memcpy(&limit,hw_mem+rel.val,2);
+    limit=laddr_read(rel.addr,2);
     cpu.idtr.limit=limit;
     if(data_size==16){
-        memcpy(&base,hw_mem+rel.val+2,3);
+        base=laddr_read(rel.addr+2,3);
         //printf("base_16=%x\n",base);
-        cpu.idtr.base=base&0x00ffffff;
+        cpu.idtr.base=base&0xffffff;
     }
     else{
-        memcpy(&base,hw_mem+rel.val+2,4);
+        base=laddr_read(rel+2,4);
         //printf("base_32=%x\n",base);
         cpu.idtr.base=base;
     }
-    return 6;
+    return len;
 }
