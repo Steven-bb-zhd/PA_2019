3fb32de3836956967e615839ca88f8c2da1a8ab2
tracer-pa-public <tracer@njuics.org>
2020-01-15 22:11:15 +0800

    >  clean 181860149 stevenzhd Linux debian-zhd-PA 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  22:11:15 up  2:46,  1 user,  load average: 0.36, 0.81, 0.68 9e125e57a0bb8795c44b4349504d4727b46019f9

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -10,7 +10,7 @@ void raise_intr(uint8_t intr_no)
 	opr_eflags.data_size=32;
 	opr_eflags.type=OPR_MEM;
 	opr_eflags.sreg=SREG_SS;
-	opr_eflags.val=cpu.eflags;
+	opr_eflags.val=cpu.eflags.val;
 	cpu.esp-=4;
 	opr_eflags.addr=cpu.esp;
 	operand_write(&opr_eflags);
@@ -18,7 +18,7 @@ void raise_intr(uint8_t intr_no)
 	opr_cs.data_size=16;
 	opr_cs.type=OPR_MEM;
 	opr_cs.sreg=SREG_SS;
-	opr_cs.val=cpu.cs;
+	opr_cs.val=cpu.cs.val;
 	cpu.esp-=2;
 	opr_cs.addr=cpu.esp;
 	operand_write(&opr_cs);
@@ -30,6 +30,22 @@ void raise_intr(uint8_t intr_no)
 	cpu.esp-=4;
 	opr_eip.addr=cpu.esp;
 	operand_write(&opr_eip);
+
+	laddr_t idt_index=cpu.idtr.base+sizeof(GateDesc)*intr_no;
+	GateDesc gate;
+	gate.val[0]=laddr_read(idt_index,4);
+	gate.val[1]=laddr_read(idt_index+4,4);
+
+	(gate.type == 0xe)
+		cpu.eflags.IF = 0;
+
+	uint32_t offset = (gate.offset_31_16 << 16) 
+						| gate.offset_15_0;
+	uint16_t selector = gate.selector;
+	cpu.cs.val = selector;
+	load_sreg((uint8_t)cpu.cs.index);
+
+	cpu.eip = offset;
 	printf("Please implement raise_intr()");
 	assert(0);
 #endif
