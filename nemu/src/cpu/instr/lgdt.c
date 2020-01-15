faeddf88bc180dc0649302b2366fa9aa0846c6e4
monitor <tracer@njuics.org>
2020-01-15 23:21:02 +0800

    git monitor commit.

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -3,25 +3,22 @@
 make_instr_func(lgdt)
 {
     OPERAND rel;
-    //int len=1;
-    rel.data_size=32;
-    rel.type=OPR_IMM;
-    rel.sreg=SREG_SS;
-    rel.addr=eip+2;
-    operand_read(&rel);
+    int len=1;
+    rel.data_size=data_size;
+    len+=modrm_rm(eip+1,&rel);
     uint16_t limit=0;
     uint32_t base=0;
-    memcpy(&limit,hw_mem+rel.val,2);
+    limit=laddr_read(rel.addr,2);
     cpu.gdtr.limit=limit;
     if(data_size==16){
-        memcpy(&base,hw_mem+rel.val+2,3);
+        base=laddr_read(rel.addr+2,3);
         //printf("base_16=%x\n",base);
         cpu.gdtr.base=base&0xffffff;
     }
     else{
-        memcpy(&base,hw_mem+rel.val+2,4);
+        base=laddr_read(rel+2,4);
         //printf("base_32=%x\n",base);
         cpu.gdtr.base=base;
     }
-    return 6;
+    return len;
 }
