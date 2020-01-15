00857eacc35e4c40dfb298e0aed7f0ab34e6096d
monitor <tracer@njuics.org>
2020-01-15 21:45:01 +0800

    git monitor commit.

---
+++ nemu/src/cpu/instr/iret.c
@@ -0,0 +1,21 @@
+#include "cpu/instr.h"
+
+make_instr_func(iret_v){
+    OPERAND opr_eip,opr_cs,opr_eflags;
+
+    opr_eip.type=OPR_MEM;
+    opr_eip.sreg=SREG_SS;
+    opr_eip.data_size=32;
+    opr_eip.addr=cpu.esp;
+    operand_read(&opr_eip);
+    cpu.esp+=4;
+    cpu.eip=opr_eip.val;
+
+    opr_cs.type=OPR_MEM;
+    opr_cs.sreg=SREG_SS;
+    opr_cs.data_size=32;
+    opr_cs.addr=cpu.esp;
+    operand_read(&opr_cs);
+    cpu.esp+=4;
+    cpu.cs=opr_cs.val&0xffff;
+}
