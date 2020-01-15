1c240673c597e2c96329d02de48d19dbeb85c23c
monitor <tracer@njuics.org>
2020-01-15 19:33:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/lidt.c
+++ nemu/src/cpu/instr/lidt.c
@@ -5,5 +5,22 @@ make_instr_func(lidt){
     OPERAND rel;
     rel.data_size=32;
     rel.type=OPR_IMM;
-    
+    rel.sreg=SREG_CS;
+    rel.addr=eip+2;
+    operand_read(&rel);
+    uint16_t limit=0;
+    uint32_t base=0;
+    memcpy(&limit,hw_mem+rel.val,2);
+    cpu.idtr.limit=limit;
+    if(data_size==16){
+        memcpy(&base,hw_mem+rel.val+2,3);
+        //printf("base_16=%x\n",base);
+        cpu.gdtr.base=base&0x00ffffff;
+    }
+    else{
+        memcpy(&base,hw_mem+rel.val+2,4);
+        //printf("base_32=%x\n",base);
+        cpu.gdtr.base=base;
+    }
+    return 6;
 }
