832b16045f9d27b8de722b2246ed48eda00a7623
monitor <tracer@njuics.org>
2020-01-15 22:53:01 +0800

    git monitor commit.

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -171,4 +171,5 @@ int int_(uint32_t eip,uint8_t opcode);
 int lidt(uint32_t eip,uint8_t opcode);
 int pusha(uint32_t eip,uint8_t opcode);
 int popa(uint32_t eip,uint8_t opcode);
+int iret(uint32_t eip,uint8_t opcode);
 #endif
