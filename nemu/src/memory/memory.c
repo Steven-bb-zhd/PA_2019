b801aeccce52794608f1738364135ec3d859e4f7
tracer-pa-public <tracer@njuics.org>
2020-01-16 22:18:53 +0800

    >  nemu 181860149 stevenzhd Linux debian-zhd-PA 4.19.0-6-686-pae #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  22:18:53 up  2:50,  1 user,  load average: 0.88, 0.54, 0.47 8606cb8d603c9b3e4dc5628a41286e031f4603e8

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -26,6 +26,7 @@ void hw_mem_write(paddr_t paddr, size_t len, uint32_t data)
 uint32_t paddr_read(paddr_t paddr, size_t len)
 {
 	uint32_t ret = 0;
+	
 	#ifdef CACHE_ENABLED
 		ret = cache_read(paddr, len, cache_block);
 	#else
